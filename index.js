c99462a3c3c7076e3d43e600c8488322a0719b24
Bright It's <brightihegworo52@gmail.com>
2020-08-25 04:07:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function shout(string) {
 function sayHiToGrandma(sentence) {
    if(sentence == "I love you, Grandma.") {
      return "I love you, too."
-   } else if(string === string.toUpperCase.()) {
+   } else if(sentence == whisper(string) ) {
      "YES INDEED!"
    }else {
      return "I love you, too."
